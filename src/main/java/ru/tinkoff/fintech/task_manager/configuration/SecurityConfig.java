62409a16c1d3cb59c2e1da42d615993faa3145ab
Askar <askar.rodin.42@gmail.com>
2021-12-05 16:28:22 +0300

    logout added

--- src/main/java/ru/tinkoff/fintech/task_manager/configuration/SecurityConfig.java
+++ src/main/java/ru/tinkoff/fintech/task_manager/configuration/SecurityConfig.java
@@ -3,21 +3,19 @@ package ru.tinkoff.fintech.task_manager.configuration;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.security.web.authentication.HttpStatusEntryPoint;
+import org.springframework.security.web.authentication.SimpleUrlAuthenticationFailureHandler;
+import org.springframework.security.web.authentication.logout.HttpStatusReturningLogoutSuccessHandler;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import ru.tinkoff.fintech.task_manager.service.CustomUserDetailsService;
 
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
 import java.util.List;
 
 @EnableWebSecurity(debug = true)
@@ -35,19 +33,23 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     public void configure(HttpSecurity http) throws Exception {
 
         http
-            .csrf().disable()
-            .cors()
-            .and().authorizeRequests()
+            .csrf().disable().cors()
+            .and()
+            .authorizeRequests()
                 .antMatchers(HttpMethod.OPTIONS, "/user").permitAll()
                 .antMatchers(HttpMethod.POST, "/user").permitAll()
-//                .antMatchers("/user/**").hasRole("ADMIN")
+                .antMatchers("/user/**").hasRole("ADMIN")
                 .anyRequest().authenticated()
-            .and().formLogin().successHandler(new AuthenticationSuccessHandler() {
-            @Override
-            public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
-                //do nothing
-            }
-        });;
+            .and()
+            .formLogin()
+                .successHandler((req, res, auth) -> res.setStatus(HttpStatus.NO_CONTENT.value()))
+                .failureHandler(new SimpleUrlAuthenticationFailureHandler())
+            .and()
+            .exceptionHandling()
+            .authenticationEntryPoint(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED))
+            .and()
+            .logout()
+            .logoutSuccessHandler(new HttpStatusReturningLogoutSuccessHandler(HttpStatus.NO_CONTENT));
     }
 
     @Bean
